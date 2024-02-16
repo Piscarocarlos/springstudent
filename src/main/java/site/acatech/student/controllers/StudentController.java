2e4a5c2c70ad6e8ab74f4fd08078eed3ea542dbc
Carlos Alognon <carlosalognon08@gmail.com>
2024-02-16 17:44:39 +0100

    Add StudentController and expose list of students request mapping

---
+++ src/main/java/site/acatech/student/controllers/StudentController.java
@@ -0,0 +1,17 @@
+package site.acatech.student.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents() {
+        return List.of("Carlos", "Keren");
+    }
+}
